5dacd47920801871b7d176e195462bdad13f5c32
Michael Schilonka <michael@blueshoe.de>
2022-04-13 16:03:51 +0200

    feat: add bug for oauth2-demo

--- oauth2-demo/app/main.py
+++ oauth2-demo/app/main.py
@@ -7,38 +7,24 @@ from fastapi import FastAPI, Header, HTTPException
 app = FastAPI()
 
 
-# Kubernetes probes
-@app.get("/alive")
-async def read_root():
-    return {"status": "alive"}
-
-
-@app.get("/ready")
-async def read_root():
-    return {"status": "ready"}
-
-
-@app.get("/startup")
-async def read_root():
-    return {"status": "startup"}
-
-
 # app routes
 @app.get("/")
-async def read_root():
+async def get_root():
     return {"Hello": "World"}
 
 
 @app.get("/me")
-async def read_token(x_forwarded_access_token: Optional[str] = Header(None)):
-    if x_forwarded_access_token:
-        # token was passed
-        data = jwt.decode(x_forwarded_access_token, options={"verify_signature": False})
-        return {"Token": data}
-    else:
-        raise HTTPException(status_code=403, detail="Token not found")
+async def get_me(x_forwarded_access_token: str = Header(None)):
+    data = jwt.decode(x_forwarded_access_token, options={"verify_signature": False})
+    return {"Token": data}
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def get_item(item_id: int, x_forwarded_access_token: Optional[str] = Header(None)):
+    if x_forwarded_access_token:
+        data = jwt.decode(x_forwarded_access_token, options={"verify_signature": False})
+        mail = data["Email"]
+        return {"item_id": item_id, "Email": mail}
+    else:
+        return {"item_id": item_id, "Email": "not given"}
+
